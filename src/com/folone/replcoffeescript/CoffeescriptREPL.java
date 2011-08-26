e524e7c3ce6dc6d6b512adac20af433f0fc44d21
Ng Tze Yang <ngty77@gmail.com>
2011-08-26 17:11:00 +0800

    WIP, stuck in infinite recursion error.

--- src/com/folone/replcoffeescript/CoffeescriptREPL.java
+++ src/com/folone/replcoffeescript/CoffeescriptREPL.java
@@ -2,9 +2,12 @@ package com.folone.replcoffeescript;
 
 import com.folone.Evaluator;
 
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.jcoffeescript.Option;
 import org.jcoffeescript.JCoffeeScriptCompiler;
+import java.util.LinkedList;
+import java.util.Collection;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -13,18 +16,37 @@ import android.os.RemoteException;
 public class CoffeescriptREPL extends Service {
 
     private final Evaluator.Stub evaluator = new Evaluator.Stub() {
-        private JCoffeeScriptCompiler coffee = new JCoffeeScriptCompiler();
-        private ScriptEngine engine = (new ScriptEngineManager()).getEngineByName("JavaScript");
+        private JCoffeeScriptCompiler coffee;
+        private Context context;
+        private Scriptable scope;
 
         public String evaluate(String script) throws RemoteException {
+          String result = "Something went wrong.";
 
-            String result = "Something went wrong.";
-            try {
-                result = engine.eval(coffee.compile(script)).toString();
-            } catch (Exception e) {
-                result = e.getMessage();
-            }
-            return result;
+          if (context == null) {
+            context = Context.enter();
+            scope = context.initStandardObjects();
+          };
+
+          try {
+              result = Context.toString(
+                  context.evaluateString(scope, compile(script), "<cmd>", 1, null)
+              );
+          } catch (Exception e) {
+              result = e.getMessage();
+          }
+
+          return result;
+        }
+
+        private String compile(String script) throws Exception {
+          if (coffee == null) {
+            Collection<Option> options = new LinkedList<Option>();
+            options.add(Option.BARE);
+            coffee = new JCoffeeScriptCompiler(options);
+          }
+
+          return coffee.compile(script);
         }
     };
 
